827df299fdc19cece0eb6c7b3bf42733f1b58abd
Montarex23 <montarex23@gmail.com>
2026-03-24 21:21:19 +0100

    movement npc + navmesh wioski

--- neurohorror/npc/nav.gd
+++ neurohorror/npc/nav.gd
@@ -1,42 +1,76 @@
 extends CharacterBody3D
 
-var movement_speed: float = 1.0
-var movement_target_position: Vector3 = Vector3(3.0,0.0,-2.0)
+var speed = 3.0
+var wander_radius = 30.0 
+var idle_time = 2.0
 
-@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+@onready var nav_agent = $NavigationAgent3D
+@onready var idle_timer = $Timer
+
+var is_idling = false
 
 func _ready():
-	# These values need to be adjusted for the actor's speed
-	# and the navigation layout.
-	navigation_agent.path_desired_distance = 0.1
-	navigation_agent.target_desired_distance = 0.2
-
-	# Make sure to not await during _ready.
-	actor_setup.call_deferred()
-
-func actor_setup():
-	# Wait for the first physics frame so the NavigationServer can sync.
-	await get_tree().physics_frame
-
-	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(movement_target_position)
-
-func set_movement_target(movement_target: Vector3):
-	navigation_agent.set_target_position(movement_target)
+	idle_timer.wait_time = idle_time
+	idle_timer.one_shot = true
+	idle_timer.timeout.connect(_on_timer_timeout)
+	start_idling()
 
 func _physics_process(delta):
-	if navigation_agent.is_navigation_finished():
+	if not is_on_floor():
+		velocity.y -= 10 * delta
+
+	if is_idling:
+		velocity.x = 0
+		velocity.z = 0
+		move_and_slide()
 		return
 
-	var current_agent_position: Vector3 = global_position
-	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+	var pox_xz = Vector2(global_position.x, global_position.z)
+	var target_pos_xz = Vector2(nav_agent.target_position.x, nav_agent.target_position.z)
+	if pox_xz.distance_to(target_pos_xz) < 1.5:
+		print("DEBUG: Idling")
+		start_idling()
+		return
+		
+	var next_path_pos = nav_agent.get_next_path_position()
+	var direction = Vector3()
+	direction.x = next_path_pos.x - global_position.x
+	direction.z = next_path_pos.z - global_position.z
+	if direction.length() > 0.01:
+		direction = direction.normalized()
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		var target_rotation = Quaternion.from_euler(Vector3(0, atan2(direction.x, direction.z), 0))
+		var current_rotation = global_transform.basis.get_rotation_quaternion()
+		var new_rotation = current_rotation.slerp(target_rotation, 10.0 * delta)
+		global_transform.basis = Basis(new_rotation)
+	else:
+		velocity.x = 0
+		velocity.z = 0
 
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
-	print(velocity)
-	velocity = Vector3(-1, 0, 1)
 	move_and_slide()
 
+func pick_random_target():
+	var map_rid = nav_agent.get_navigation_map()
+	
+	var random_offset = Vector3(
+		randf_range(-wander_radius, wander_radius), 
+		0, 
+		randf_range(-wander_radius, wander_radius)
+	)
+	var random_target = global_position + random_offset
+	var correct_target = NavigationServer3D.map_get_closest_point(map_rid, random_target)
+	
+	# Ustawiamy nowy cel i wyłączamy stan odpoczynku
+	nav_agent.target_position = correct_target
+	is_idling = false
+	print("DEBUG: ", correct_target)
 
-func _on_change_target_timer_timeout() -> void:
-	print("Target change")
-	set_movement_target(Vector3(randf_range(-3.0, 3.0), 0, randf_range(-3.0, 3.0)))
+func start_idling():
+	is_idling = true
+	velocity.x = 0
+	velocity.z = 0
+	idle_timer.start()
+
+func _on_timer_timeout():
+	pick_random_target()
